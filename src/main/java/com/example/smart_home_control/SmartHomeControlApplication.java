ec3cbfb2865063ea9b65d0eb93b4a0227418738b
Junxiang Hao <jun.junxiang@medavie.bluecross.ca>
2025-04-25 20:33:11 -0300

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/example/smart_home_control/SmartHomeControlApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_home_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomeControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHomeControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smart_home_control/SmartHomeControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smart_home_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
